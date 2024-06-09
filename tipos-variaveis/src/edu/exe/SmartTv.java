585352fce2a65c1ec487649dfbe8834d39bf7d42
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-09 09:21:43 -0300

    Exercicio Metodos

---
+++ tipos-variaveis/src/edu/exe/SmartTv.java
@@ -0,0 +1,23 @@
+package edu.exe;
+
+public class SmartTv {
+    public void ligado(){
+
+    }
+
+    public void ligar(){
+
+    }
+
+    public void desligar(){
+
+    }
+
+    public void volume(int num1, int num2){
+
+    }
+
+    public void canal(int canal){
+        
+    }
+}

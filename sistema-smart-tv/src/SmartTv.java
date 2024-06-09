a7c077016db6c021f1871817f599127999e4139e
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-09 17:57:14 -0300

    melhoria Metodo

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,59 +1,76 @@
 public class SmartTv {
-    
-    boolean ligada=false;
-    int canal=1;
-    int volume=25;
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
 
-    public void ligar(){
-        ligada=true;
+    public void ligar() {
+        ligada = true;
+        System.out.println("TV ligada.");
     }
 
-    public void desligar(){
-        ligada=false;
+    public void desligar() {
+        ligada = false;
+        System.out.println("TV desligada.");
     }
 
-    public void aumentarVolume(){
-        if (ligada==true) {
-            volume++;
-            System.out.println("aumentando volume para: " + volume);
-        }
-        else{
-            System.out.println("TV desligada");
-        }
-        
-    }
-    
-    public void diminuirVolume(){
-        if (ligada==true) {
-            volume--;
-            System.out.println("diminuindo volume para: " + volume);
-        }
-        else{
+    public void aumentarVolume() {
+        if (ligada) {
+            if (volume < 100) {
+                volume++;
+                System.out.println("Aumentando volume para: " + volume);
+            } else {
+                System.out.println("Volume já está no máximo.");
+            }
+        } else {
             System.out.println("TV desligada");
         }
     }
 
-    public void diminuirCanal(){
-        if (ligada==true) {
-            canal--;
-            System.out.println("Canal: " + canal);
-        }
-        else{
+    public void diminuirVolume() {
+        if (ligada) {
+            if (volume > 0) {
+                volume--;
+                System.out.println("Diminuindo volume para: " + volume);
+            } else {
+                System.out.println("Volume já está no mínimo.");
+            }
+        } else {
             System.out.println("TV desligada");
         }
     }
 
-    public void aumentarCanal(){
-        if (ligada==true) {
+    public void aumentarCanal() {
+        if (ligada) {
             canal++;
             System.out.println("Canal: " + canal);
-        }
-        else{
+        } else {
             System.out.println("TV desligada");
         }
     }
 
-    public void mudarCanal(int novoCanal){
-        canal = novoCanal;
+    public void diminuirCanal() {
+        if (ligada) {
+            if (canal > 1) {
+                canal--;
+                System.out.println("Canal: " + canal);
+            } else {
+                System.out.println("Você já está no canal mínimo.");
+            }
+        } else {
+            System.out.println("TV desligada");
+        }
+    }
+
+    public void mudarCanal(int novoCanal) {
+        if (ligada) {
+            if (novoCanal >= 1 && novoCanal <= 100) {
+                canal = novoCanal;
+                System.out.println("Mudando para o canal: " + canal);
+            } else {
+                System.out.println("Canal inválido.");
+            }
+        } else {
+            System.out.println("TV desligada");
+        }
     }
 }

2a79f8324a8de9699bc6b7097ca19e724c0909b3
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-09 22:19:17 -0300

    To do

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,15 @@
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usaurio
+
+        //Obter pela classe Scanner os valores digitados no terminal 
+
+        //Exibir a mensagem final da conta criada
     }
 }

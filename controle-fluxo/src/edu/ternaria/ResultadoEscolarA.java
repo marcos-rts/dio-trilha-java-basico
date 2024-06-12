f93aaa8be048e9815282b349aa39786ad54cdae3
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-06-12 00:14:23 -0300

    condicional ternaria

---
+++ controle-fluxo/src/edu/ternaria/ResultadoEscolarA.java
@@ -0,0 +1,9 @@
+package edu.ternaria;
+
+public class ResultadoEscolarA {
+    public static void main(String[] args) {
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+    }
+}
